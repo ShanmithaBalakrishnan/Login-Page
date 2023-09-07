627d0866ffef3476eb19ad691e575502b09383ba
shanmitha <sanmithasree22@gmail.com>
2023-09-07 09:58:26 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useState } from "react";
+import "./App.css";
+import { Login } from '@mui/icons-material';
+
+function App() {
+  return (
+    <Login/>
+  );
+}
+
+export default App;
---
+++ src/Components/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container, Grid, Paper } from "@mui/material";
+
+const Login = () => {
+  return (
+    <>
+      <div>
+        <p>WelcomeTo</p>
+        <h1>KarmaDev.In</h1>
+      </div>
+    </>
+  );
+};
